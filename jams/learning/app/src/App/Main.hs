a6e7fa10faf0be3a2f1d154e11f0e4164a215c13
Jack Viers <jackcviers@gmail.com>
2013-07-11 22:46:47 -0500

    Implements csv parsing and extraction

--- jams/learning/app/src/App/Main.hs
+++ jams/learning/app/src/App/Main.hs
@@ -4,11 +4,31 @@ import Data.Either
 import Data.Function
 import Text.CSV
 
+type CSVs = (CSV, CSV)
+type CSVEithers = ((Either Text.Parsec.Error.ParseError CSV), (Either Text.Parsec.Error.ParseError CSV))
+type CSVIOs = (IO (Either Text.Parsec.Error.ParseError CSV), IO (Either Text.Parsec.Error.ParseError CSV))
+type Samples = (FilePath, FilePath)
+
 main :: IO ()
 main = print "test"
 
 -- euclidean
 -- knn
--- parseToSamples
+-- parseFromFiles
+parseFromFiles :: Samples -> CSVIOs
+parseFromFiles (training, example) = ((parseCSVFromFile training), (parseCSVFromFile example))
+
+-- extractCSVEithers
+extractCSVEithers :: CSVIOs -> CSVEithers
+extractCSVEithers (training, example) = do x <- training
+                                                y <- example
+                                                return (x,y)
+-- eithersToCSVs
+eithersToCSVs :: CSVEithers -> CSVs
+eithersToCSVs (training, sample) = (x, y)
+                                   where
+                                     x = either (error . show) id training
+                                     y = either (error' . show) id example
+
 -- vote
 -- confidence
